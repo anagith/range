d9b1bf6fb97d0c0a143d700a039a8a5715ec9737
Anahiit <amartirosyan904@gmail.com>
2021-04-30 11:40:18 +0400

    added spaces and arrow

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,17 +9,16 @@ public class Main {
         int number = scanner.nextInt();
         switch ((number + 100) / 100) {
             case 1:
-                System.out.println("number is in range 0-100");
+                System.out.println("number is in range 0 -> 100");
                 break;
             case 2:
-                System.out.println("number is in range 100-200");
+                System.out.println("number is in range 100 -> 200");
                 break;
             case 3:
-                System.out.println("number is in range 200-300");
+                System.out.println("number is in range 200 -> 300");
                 break;
             default:
                 System.out.println("number is out of range");
-                
         }
     }
 }
