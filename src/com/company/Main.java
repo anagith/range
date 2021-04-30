7553503b20702167ffa4af00344b81e5299e5ef3
Anahiit <amartirosyan904@gmail.com>
2021-04-30 11:14:41 +0400

    fixed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,15 +7,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        switch ((number + 45) / 45) {
+        switch ((number + 100) / 100) {
             case 1:
-                System.out.println("number is in range 0-45");
+                System.out.println("number is in range 0-100");
                 break;
             case 2:
-                System.out.println("number is in range 45-90");
+                System.out.println("number is in range 100-200");
                 break;
             case 3:
-                System.out.println("number is in range 90-135");
+                System.out.println("number is in range 200-300");
                 break;
             default:
                 System.out.println("number is out of range");
